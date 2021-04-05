fb1376ad80f26a74f438cddf0702e1d0f8199493
Swapnanil Halder <43921638+SwapnanilHalder@users.noreply.github.com>
2021-04-05 20:37:38 +0530

    5th April, 2021

--- CodeChef/Template.cpp
+++ CodeChef/Template.cpp
@@ -1,27 +1,5 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include<unordered_map>
-#include <fstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <utility>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iomanip>
-#include <cassert>
-#define SPEED ios_base::sync_with_stdio(false)
-#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#include<bits/stdc++.h>
+#define SPEED ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 #define endl '\n'
 #define vi vector<int>
 #define vi2d vector<vector<int>> 
@@ -31,24 +9,15 @@
 #define pi 3.1415926535897932384626
 #define MOD 1000000007
 #define ll long long
-#define int long long int
+#define lli long long int
 #define END cout<<"\n"
 #define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
 #define pv(v) for(const auto &i: v){cout<<i<<" ";}END
 #define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
-#define deb(x) cout << '\t'<<#x << " = " << x << endl
-#define inpt(x) int x{0}; cin >> x
+#define deb(x) cout << #x << "=" << x << endl
 #define pl(x) cout << x << endl
-#define all(v) v.begin(),v.end()
-#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
-#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " :  " << itr->second;END;}
-#define timestart auto start = chrono::high_resolution_clock::now()
-#define timestop auto stop = chrono::high_resolution_clock::now();auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);cout << "Time taken by function: "<< duration.count() << " microseconds" << endl
 using namespace std;
 int gcd(int a, int b){return b ? gcd(b, a%b): a;}
-int lcm(int a, int b){return (a*b)/gcd(a,b);}
-template <class T> T Max(T x,T y){return (x>y)?x:y;}
-template <class T> T Min(T x,T y){return (y>x)?x:y;}
 
 vi input1l(){
     vi input(0);
@@ -56,19 +25,12 @@ vi input1l(){
     getline(cin, str);
     stringstream X(str);
     while(getline(X, num, ' ')) {
-        input.pb(stoll(num));
+        input.pb(stoi(num));
     }
     //pv(input);
     return input;
 }
 
-void sol() {
-    
-}
-
-int32_t main() {
-    inpt(t);
-    while(t--) {
-        sol();
-    }
+int main() {
+    pl(gcd(15, 25));
 }
