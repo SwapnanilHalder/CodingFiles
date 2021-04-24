cec0b7d285a32936d10c44293ada417dcc345933
SwapnanilHalder <halderswapnanil@gmail.com>
2021-04-24 19:16:50 +0530

    24th April, 2021

---
+++ CodeForces/Round_717_Div_2/C_Baby_Ehab_Partitions_Again.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <numeric>
+#define pi 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 9223372036854775800 
+#define ll long long
+#define int long long int
+#define END cout<<"\n"
+#define SPEED ios_base::sync_with_stdio(false)
+#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+#define vi vector<int>
+#define vi2d vector<vector<int>> 
+#define pb push_back
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define ford(i,a,b) for(int i=a;i>=b;i--)
+#define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define pv(v) for(const auto &i: v){cout<<i<<" ";}END
+#define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
+#define deb(x) cout << '\t'<<#x << " = " << x << endl
+#define inpt(x) int x{0}; cin >> x
+#define pl(x) cout << x << endl
+#define all(v) v.begin(),v.end()
+#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " ->  " << itr->second;END;}
+#define pmpair(v) cout<< #v<<endl; cout << "	(" << v.first << " , " << v.second << ")";END
+using namespace std;
+
+vi input1l(){
+    vi input(0);
+    string str, num;
+    getline(cin, str);
+    stringstream X(str);
+    while(getline(X, num, ' ')) {
+        input.pb(stoll(num));
+    }
+    //pv(input);
+    return input;
+}
+
+bool findPartiion(vi arr, vector<vector<bool>> &part, int n, int sum)
+{
+    int i, j;
+    // initialize top row as true
+    for (i = 0; i <= n; i++)
+        part[0][i] = true;
+ 
+    // initialize leftmost column,
+    // except part[0][0], as 0
+    for (i = 1; i <= sum / 2; i++)
+        part[i][0] = false;
+ 
+    // Fill the partition table in bottom up manner
+    for (i = 1; i <= sum / 2; i++) {
+        for (j = 1; j <= n; j++) {
+            part[i][j] = part[i][j - 1];
+            if (i >= arr[j - 1])
+                part[i][j] = part[i][j]
+                             || part[i - arr[j - 1]][j - 1];
+        }
+    }
+ 
+    /* // uncomment this part to print table
+    for (i = 0; i <= sum/2; i++)
+    {
+    for (j = 0; j <= n; j++)
+        cout<<part[i][j];
+    cout<<endl;
+    } */
+    pv2d(part);
+    return part[sum / 2][n];
+}
+
+void sol() {
+    inpt(n); cinbuffer;
+    vi arr = input1l();
+    int sums = accumulate(all(arr), 0);
+    if (sums % 2 != 0 ) {
+        pl("0"); return;
+    }
+    vector<vector<bool>> part((sums/2+1) , vector<bool> (n+1, 0)); //         [sum / 2 + 1][n + 1];
+    if ( findPartiion(arr, part, n, sums) == false) {
+        pl("0"); return;
+    }
+    For(i, 0, n) {
+        if (arr[i] % 2 != 0) {
+            pl("1");
+            pl(i+1); return;
+        }
+    }
+    ford(i, sums/2, 1) {
+        if (part[i][n] == 0) {
+            if(part[(n/2) - i][n] == 1){
+                
+            }
+        }
+    }
+
+}
+
+int32_t main() {
+    SPEED;
+    sol();
+}
