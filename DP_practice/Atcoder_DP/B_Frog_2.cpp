2e0f60a80ee2745e2bcea1f3f39321f04f6011a8
SwapnanilHalder <halderswapnanil@gmail.com>
2021-04-30 19:52:20 +0530

    30th April, 2021

---
+++ DP_practice/Atcoder_DP/B_Frog_2.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <fstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <utility>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <cassert>
+#include <chrono>
+#include <numeric>
+#define SPEED ios_base::sync_with_stdio(false)
+#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+#define vi vector<int>
+#define vi2d vector<vector<int>> 
+#define li list<int>
+#define mii map<int, int>
+#define msi map<string, int>
+#define pb push_back
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define ford(i,a,b) for(int i=a;i>=b;i--)
+#define pi 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 9223372036854775800 
+#define ll long long
+#define int long long int
+#define END cout<<"\n"
+#define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define pv(v) for(const auto &i: v){cout<<i<<" ";}END
+#define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
+#define deb(x) cout << '\t'<<#x << " = " << x << endl
+#define inpt(x) int x{0}; cin >> x
+#define pl(x) cout << x << endl
+#define all(v) v.begin(),v.end()
+#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " ->  " << itr->second;END;}
+#define pmpair(v) cout<< #v<<endl; cout << "	(" << v.first << " , " << v.second << ")";END
+#define timestart auto start = chrono::high_resolution_clock::now()
+#define timestop auto stop = chrono::high_resolution_clock::now();auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);cout << "Time taken by function: "<< duration.count() << " microseconds" << endl
+using namespace std;
+int gcd(int a, int b){return b ? gcd(b, a%b): a;}
+int lcm(int a, int b){return (a*b)/gcd(a,b);}
+template <class T> T Max(T x,T y){return (x>y)?x:y;}
+template <class T> T Min(T x,T y){return (y>x)?x:y;}
+
+vi input1l(){
+    vi input(0);
+    string str, num;
+    getline(cin, str);
+    stringstream X(str);
+    while(getline(X, num, ' ')) {
+        input.pb(stoll(num));
+    }
+    //pv(input);
+    return input;
+}
+
+int32_t main() {
+    // system("clear");
+    inpt(n);inpt(k);cinbuffer;
+    vi arr = input1l();
+    vi2d dp(k+1, vi(n+1, INT_MAX));
+    dp[0][1] = 0;
+    For(i, 1, k+1) {
+        dp[i][1] = abs(arr[i]-arr[0]);
+    }
+    // dp[1][1] = abs(arr[1] - arr[0]);
+    // dp[2][1] = abs(arr[2]-arr[0]);
+    int temp = 0;
+    For(i, 2, n+1) {
+        dp[0][i] = dp[1][i-1];
+        For(j, 1, k+1) {
+            // cout << "Now i : "<<i<< " ; j : "<<j<<endl;
+            if(i <= n-j) {
+                if(j == k) {
+                    dp[j][i] = dp[0][i] + abs(arr[i-1+j] - arr[i-1]);
+                }
+                else {
+                    temp = dp[0][i] + abs(arr[i-1+j] - arr[i-1]);
+                    dp[j][i] = min(temp, dp[j+1][i-1]);
+                }
+            }
+
+        }
+        
+        // if(i<=n-1) dp[1][i] = dp[0][i] + abs(arr[i]-arr[i-1]);
+        // if(i<= n-2) dp[2][i] = dp[0][i] + abs(arr[i+1] - arr[i-1]);
+    }
+    // pv2d(dp);
+    cout << (dp[0][n]) << endl;
+}
