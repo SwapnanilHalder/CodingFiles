d924845930b55b79d6a642e5d3fb798390b534c9
Swapnanil Halder <43921638+SwapnanilHalder@users.noreply.github.com>
2021-05-16 20:10:01 +0530

    16th May, 2021

---
+++ CodeForces/Educational_Round_109/B_Permutation_Sort1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <numeric>
+#define pi 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 9223372036854775800 
+#define ll long long
+#define int long long int
+#define END cout<<"\n"
+#define SPEED ios_base::sync_with_stdio(false)
+#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define endl '\n'
+#define vi vector<int>
+#define vi2d vector<vector<int>> 
+#define pb push_back
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define ford(i,a,b) for(int i=a;i>=b;i--)
+#define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define pv(v) for(const auto &i: v){cout<<i<<" ";}END
+#define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
+#define deb(x) cout << '\t'<<#x << " = " << x << endl
+#define inpt(x) int x{0}; cin >> x
+#define pl(x) cout << x << endl
+#define all(v) v.begin(),v.end()
+#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " ->  " << itr->second;END;}
+#define pmpair(v) cout<< #v<<endl; cout << "	(" << v.first << " , " << v.second << ")";END
+using namespace std;
+
+vi input1l(){
+    vi input(0);
+    string str, num;
+    getline(cin, str);
+    stringstream X(str);
+    while(getline(X, num, ' ')) {
+        input.pb(stoll(num));
+    }
+    //pv(input);
+    return input;
+}
+
+void sol() {
+    inpt(k); cinbuffer;
+    vi arr = input1l();
+    int n = arr.size();
+    int one=0, end = 0, sorted = 1;
+    For(i, 0, n) {
+        if(arr[i] != i+1) {
+            sorted = 0;
+        }
+        if(arr[i] == 1) {
+            one = i;
+        }
+        if(arr[i] == n) {
+            end = i;
+        }
+    }
+    // deb(one); deb(end);
+    if(sorted == 1) {
+        pl("0");
+        return;
+    }
+    if(one == n-1 && end == 0 ) {
+        pl("3");
+        return;
+    }
+    // if(one == n-1 || end == 0) {
+    //     pl("2");
+    //     return;
+    // }
+    if(one == 0 || end == n-1) {
+        pl("1");
+        return;
+    }
+    pl("2");
+    return;
+
+}
+
+int32_t main() {
+    SPEED;
+    inpt(t); 
+    while(t--) {
+        sol();
+    }
+}
