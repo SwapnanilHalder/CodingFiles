0d6e573caf9813ee836543ae7d2b849c5056a512
Swapnanil Halder <43921638+SwapnanilHalder@users.noreply.github.com>
2021-05-16 22:16:40 +0530

    16th May, 2021

--- CodeChef/May_Challenge_2021/KKLING.cpp
+++ CodeChef/May_Challenge_2021/KKLING.cpp
@@ -16,7 +16,7 @@
 #define MOD 1000000007
 #define INF 9223372036854775800 
 #define ll long long
-#define int long long int
+#define int long long
 #define END cout<<"\n"
 #define SPEED ios_base::sync_with_stdio(false)
 #define SPEED1 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
@@ -64,7 +64,7 @@ void buildTree(TreeNode *root, vi2d &adj, vector<bool> &visited) {
     For(i, 0, adj[root->data].size()) {
         if(! visited[adj[root->data][i]]) {
             TreeNode *temp = new TreeNode (adj[root->data][i]);
-            (root->child).pb(temp);
+            root->child.pb(temp);
         }
     }
     For(i, 0, root->child.size()) {
@@ -75,16 +75,98 @@ void buildTree(TreeNode *root, vi2d &adj, vector<bool> &visited) {
 void DFS(TreeNode *root, int l) {
     root->lvl = l;
     For(i, 0, root->child.size()) {
-        
+        TreeNode *current = root->child[i];
+        DFS(current, l+1);
     }
 }
 
-void sol() {
-    
+vector<pair<int, int>> leaves;
+
+void DFS2(TreeNode *root) {
+    For(i, 0, root->child.size()) {
+        TreeNode *current = root->child[i];
+        DFS2(current);
+    }
+    if(root->child.size() == 0) {
+        leaves.pb({root->lvl, root->data + 1});
+    }
+}
+
+void sol () {
+    inpt(n);
+    vi2d adj(n);
+    For(i, 0, n-1) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    TreeNode *root = new TreeNode(0);
+    vector<bool> visited(n, 0) ;
+    buildTree(root, adj, visited);
+    DFS(root, 0);
+    vector<pair<int, int>> ans;
+    For(i, 0, root->child.size()) {
+        DFS2(root->child[i]);
+        vector<pair<int, int>> v;
+        pair<int, int> p;
+        bool f = 0;
+        sort(all(leaves));
+        p = leaves[0];
+        v.pb(p);
+        int val = p.first;
+        int j = 1;
+        while(j < leaves.size()) {
+            while(j < leaves.size() && leaves[i].first == val) {
+                if (f == 1) {
+                    v.pb({p.first + 1, leaves[j].second});
+                }
+                else {
+                    v.pb(leaves[j]);
+                }
+                j++;
+            }
+            if (j < leaves.size() ) {
+                v.clear();
+                f = 1;
+                p.second = leaves[j].second;
+                v.pb({p.first+1, leaves[j].second});
+                val = leaves[j].first;
+                j++;
+            }
+        }
+        leaves.clear();
+
+        for(auto x : v) {
+            ans.pb(x);
+        }
+    }
+
+    sort(all(ans));
+    pl("ANS : ");
+    loop(it, ans) {
+        cout << it->first << " , " << it->second <<"\n";
+    }
+    int day = ans[ans.size()-1].first;
+    deb(day);
+    vi AA;
+    For(i, 0, ans.size()) {
+        deb(ans[i].first);
+        if(ans[i].first == day) {
+            AA.pb(ans[i].second);
+            pl(ans[i].second);
+        }
+        // else {
+        //     break;
+        // }
+    }
+    cout << AA.size() << " " << day << "\n";
+    pv(AA);
 }
 
 int32_t main() {
-    SPEED;
+    SPEED1;
     inpt(t); 
     while(t--) {
         sol();
