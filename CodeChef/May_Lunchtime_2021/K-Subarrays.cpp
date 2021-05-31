ea92a8e86a680bc330b9b4db0e1c8fb55ce784cb
SwapnanilHalder <halderswapnanil@gmail.com>
2021-05-31 22:20:42 +0530

    31st May, 2021

---
+++ CodeChef/May_Lunchtime_2021/K-Subarrays.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <numeric>
+#define pi 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 9223372036854775800 
+#define ll long long
+#define int long long int
+#define SPEED ios_base::sync_with_stdio(false)
+#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl '\n'
+#define vi vector<int>
+#define vi2d vector<vector<int>> 
+#define pb push_back
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define ford(i,a,b) for(int i=a;i>=b;i--)
+#define END cout << "\n" 
+#define ENDstd printf("\n")
+#define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define pv(v) for(const auto &i: v){cout<<i<<" ";}END
+#define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
+#define deb(x) cout << '\t'<<#x << " = " << x << endl
+#define inpt(x) int x{}; cin >> x
+#define inptstd(x) int x{}; scanf("%lld", &x)
+#define pn(x) printf("%lld\n", x)
+#define pl(x) cout << x << endl
+#define all(v) v.begin(),v.end()
+#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " ->  " << itr->second;END;}
+#define pmpair(v) cout<< #v<<endl; cout << "	(" << v.first << " , " << v.second << ")";END
+using namespace std;
+
+vi input1l(){
+    vi input(0);
+    string str, num;
+    getline(cin, str);
+    stringstream X(str);
+    while(getline(X, num, ' ')) {
+        input.pb(stoll(num));
+    }
+    //pv(input);
+    return input;
+}
+
+int maxSubArraySum(vi &a)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    For(i, 0, a.size()) {
+        deb(i); deb(max_ending_here); deb(max_so_far);
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+vi store;
+
+int maxSubArrayStoreSum(vi &a)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    For(i, 0, a.size()) {
+        deb(max_ending_here); deb(max_so_far);
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+void sol() {
+    inpt(n); inpt(k); cinbuffer;
+    vi arr = input1l();
+    if(k == 1) {
+        int ans = maxSubArraySum(arr);
+        pl(ans); return;
+    }
+    int ended = n;
+    int ind = n-1;
+    int pos_sum = -((1<<31) - 1);
+    int max_sum = -((1<<31) - 1);
+    store.clear();
+    store = vi(k+1, 0);
+    ford(multiplicity, k, 1) {
+        ind = ended-1;
+        store[k] = arr[ind];
+        for(;ind >= )
+    }
+}
+
+int32_t main() {
+    SPEED1;
+    inpt(t); 
+    while(t--) {
+        sol();
+    }
+}
+
+
+/*
+
+2
+5 1
+1 2 -1 3 1
+5 1
+-1 2 11 -23 12
+
+*/
---
+++ CodeChef/May_Lunchtime_2021/Total_Components.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <numeric>
+#define pi 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 9223372036854775800 
+#define ll long long
+#define int long long int
+#define SPEED ios_base::sync_with_stdio(false)
+#define SPEED1 ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl '\n'
+#define vi vector<int>
+#define vi2d vector<vector<int>> 
+#define pb push_back
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define ford(i,a,b) for(int i=a;i>=b;i--)
+#define END cout << "\n" 
+#define ENDstd printf("\n")
+#define cinbuffer cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define pv(v) for(const auto &i: v){cout<<i<<" ";}END
+#define pv2d(v2d) for(const auto &i : v2d) {pv(i);}cout<<""
+#define deb(x) cout << '\t'<<#x << " = " << x << endl
+#define inpt(x) int x{}; cin >> x
+#define inptstd(x) int x{}; scanf("%lld", &x)
+#define pn(x) printf("%lld\n", x)
+#define pl(x) cout << x << endl
+#define all(v) v.begin(),v.end()
+#define loop(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define pmint(v) cout<< #v<<endl;loop(itr, v){cout << "\t" << itr->first << " ->  " << itr->second;END;}
+#define pmpair(v) cout<< #v<<endl; cout << "	(" << v.first << " , " << v.second << ")";END
+using namespace std;
+
+vi input1l(){
+    vi input(0);
+    string str, num;
+    getline(cin, str);
+    stringstream X(str);
+    while(getline(X, num, ' ')) {
+        input.pb(stoll(num));
+    }
+    //pv(input);
+    return input;
+}
+
+// vi primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599 , 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 };
+
+vi primes;
+
+void SieveOfEratosthenes(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+ 
+    for (int p = 2; p <= n; p++) {
+        primes[p] = primes[p-1];
+        if (prime[p]) {
+            primes[p] += 1; 
+        }
+    }
+    // pv(primes);
+        
+}
+
+
+void sol(int n) {
+    if(n == 2) {
+        pl("1"); return;
+    }
+    if(n == 3) {
+        pl("2"); return;
+    }
+    int ans = primes[n] - primes[n/2] + 1;
+    // int primesin = 0;
+    // int ans = 0;
+    // For(i, 0, primes.size()) {
+    //     if (primes[i] <= n) {
+    //         primesin++;
+    //         if(n / primes[i] < 2) {
+    //             ans++;
+    //         }
+    //     }
+    //     else {
+    //         break;
+    //     }
+    // } 
+    // deb(primesin);
+    
+    pl(ans); return;
+}
+
+int32_t main() {
+    SPEED1 ;
+    inpt(t); 
+    vi arr;
+    int x;
+    For(i, 0, t) {
+        cin >> x;
+        arr.pb(x);
+    }
+    int maxm = *max_element(all(arr));
+    primes.clear();
+    primes = vi(maxm+1, 0);
+    SieveOfEratosthenes(maxm);
+    For(i, 0, t) {
+        sol(arr[i]);
+    }
+}
