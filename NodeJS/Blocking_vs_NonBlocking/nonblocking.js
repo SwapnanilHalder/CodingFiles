7f7c1e2e80dec34dc689f598455efc1f3ce6b7b5
Swapnanil Halder <halderswapnanil@gmail.com>
2021-09-13 11:06:21 +0530

    13th Sept, 2021

---
+++ NodeJS/Blocking_vs_NonBlocking/nonblocking.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+fs.readFile('text.txt', function (err) {
+    if(err) {
+        console.log(err);
+    }
+    setTimeout(() => {
+        console.log("Display after 1 second");
+    }, 1000);
+});
+console.log("Start here");ss
