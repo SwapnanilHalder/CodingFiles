52f5419f687a3f40c02c9ddd993bb32b16893452
Swapnanil Halder <halderswapnanil@gmail.com>
2021-11-28 12:51:37 +0530

    28th Nov, 2021

---
+++ cp_practice/Exam/m.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+
+public class m {
+    public static void main(String[] args) {
+    Teacher t = new Student();
+    System.out.println(t.name);
+    }
+}
+    class Teacher {
+    String name="Sam" ;
+    }
+    class Student{
+        String name="Kevin";
+    }
