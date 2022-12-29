1a4f2a2978bec0dcb7b3df890a0e8054fd3f3aff
Swapnanil Halder <43921638+SwapnanilHalder@users.noreply.github.com>
2022-12-29 10:52:41 +0530

    29th Dec, 2022

--- script_backups/macos_a.sh
+++ script_backups/macos_a.sh
@@ -1,5 +1,13 @@
 cp ~/library/preferences/com.apple.dock.plist ~/com.apple.dock.plist
+echo "Running brew update & upgrade"
 brew update && brew upgrade && brew upgrade --cask && brew upgrade --greedy
+echo "Showing all formulae that were only installed as a dependency of another formula and are now no longer needed."
+brew autoremove -n
+echo "to remove these, run brew autoremove"
+echo "Running brew cleanup"
+brew cleanup
+echo "Running brew doctor"
+brew doctor
 defaults import com.apple.dock.plist ~/com.apple.dock.plist
 killall Dock
 rm ~/com.apple.dock.plist
